d34120e71d59d793262439afb41615092c2acfaa
Armando del Río <adrio.coder@gmail.com>
2021-11-15 16:54:52 -0600

    add implementation of deposit method on type wallet

--- pointers/v1/wallet.go
+++ pointers/v1/wallet.go
@@ -2,15 +2,25 @@ package wallet
 
 import "fmt"
 
-type Wallet struct {
-	balance int
-}
+type (
+	Wallet struct {
+		balance Bitcoin
+	}
+	Bitcoin  int
+	Stringer interface {
+		String() string
+	}
+)
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
