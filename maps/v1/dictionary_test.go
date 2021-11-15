e149005f83bf4c578f2d78c83131bcc8801eda7f
Armando del Río <adrio.coder@gmail.com>
2021-11-15 17:51:47 -0600

    add dictionaty implementation using maps

---
+++ maps/v1/dictionary_test.go
@@ -0,0 +1,19 @@
+package dictionary
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
---
+++ maps/v2/dictionary.go
@@ -0,0 +1,7 @@
+package dictionary
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) string {
+	return d[word]
+}
