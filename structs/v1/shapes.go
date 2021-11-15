b89fea7cac70bebe6f77b0930635447a4a2d05a2
Armando del Río <adrio.coder@gmail.com>
2021-11-15 15:21:00 -0600

    add methods for area

---
+++ structs/v1/shapes.go
@@ -0,0 +1,24 @@
+package shapes
+
+import "math"
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return (r.Width * r.Height)
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
