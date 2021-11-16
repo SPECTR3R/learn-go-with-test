a5e86f08bd0c674838ed6505424d48d259722208
Armando del Río <adrio.coder@gmail.com>
2021-11-15 19:06:33 -0600

    add dictionaty test for updating exiting words

---
+++ maps/v6/dictionary.go
@@ -0,0 +1,48 @@
+package dictionary
+
+type Dictionary map[string]string
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrAlreadyDefined   = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word string, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrAlreadyDefined
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word string, definition string) error {
+	_, err := d.Search(word)
+
+	if err == nil {
+		d[word] = definition
+		return nil
+	}
+	return ErrWordDoesNotExist
+}
