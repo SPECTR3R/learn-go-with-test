4292cf63ac7d305c04ffb5ff5f0f9d80b771553a
Armando del Río <adrio.coder@gmail.com>
2021-11-16 23:34:27 -0600

    add test for posting data, refactor

--- http-server/v2/server.go
+++ http-server/v2/server.go
@@ -15,11 +15,22 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, r)
+	case http.MethodGet:
+		p.showScore(w, r)
+	}
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		w.WriteHeader(http.StatusAccepted)
 		return
 	}
+}
 
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	score := p.store.GetPlayerScore(player)
 	if score == 0 {
