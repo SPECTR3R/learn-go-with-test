cc7d43d4f5cbe006f223ae40f111098d7042c4b9
Armando del Río <adrio.coder@gmail.com>
2021-11-17 11:03:46 -0600

    add router

--- http-server/v2/server.go
+++ http-server/v2/server.go
@@ -26,15 +26,15 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
-	p.store.RecordWin(player)
-	w.WriteHeader(http.StatusAccepted)
-}
-
 func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
 	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
 }
